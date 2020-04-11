1bdb4d78176f428bb3253767bde8ab8cc089e22e
Mike Wazovzky <mike.wazovzky@gmail.com>
2020-04-11 10:14:39 +0300

    Phone validation

--- app/Http/Controllers/Api/UsersPhonesController.php
+++ app/Http/Controllers/Api/UsersPhonesController.php
@@ -2,10 +2,11 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
-use App\Http\Resources\PhoneResource;
 use App\Models\User;
+use App\Http\Resources\PhoneResource;
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class UsersPhonesController extends Controller
 {
@@ -17,10 +18,16 @@ class UsersPhonesController extends Controller
     public function update(User $user, Request $request)
     {
         $attributes = $request->validate([
-            'phones' => ['sometimes', 'array'],
-            'phones.*.number' => ['required', 'string'],
+            'phones' => [
+                'sometimes',
+                'array'
+            ],
+            'phones.*.number' => [
+                'required',
+                'phone',
+                Rule::unique('phones', 'number')->ignore($user->id, 'user_id'),
+            ],
         ]);
-
 
         $user->phones()->delete();
         $user->phones()->createMany($attributes['phones']);
